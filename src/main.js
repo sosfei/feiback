cc75b8a50c5b24a4bc5401475582d06be144deb8
sosfei <zingggum@outlook.com>
2023-12-17 22:05:23 +0800

    Update UpFile

--- src/main.js
+++ src/main.js
@@ -4,7 +4,10 @@ import Axios from 'axios'
 import App from '@/App.vue'
 import Router from '@/router'
 
+import UpFiles from '@/components/UpFiles.vue'
+
 Axios.defaults.baseURL = 'http://localhost:8881'
 const app = createApp(App)
 app.use(Router)
 app.mount('#app')
+app.component('UpFiles', UpFiles)
--- src/pages/home/test.vue
+++ src/pages/home/test.vue
@@ -1,20 +1,5 @@
-<template>
-    <ElCard style="width: 80%; margin: 80px auto" header="文件分片上传">
-        <ElUpload class="upload-demo" drag action="/" multiple :http-request="handleHttpRequest"
-            :on-remove="handleRemoveFile">
-            <el-icon class="el-icon--upload">
-                <IEpUploadFilled />
-            </el-icon>
-            <!-- <img v-if="imageUrl" :src="imageUrl" class="avatar" />
-            <el-icon v-else class="avatar-uploader-icon">
-                <Plus />
-            </el-icon> -->
-            <div class="el-upload__text">
-                请拖拽文件到此处或 <em>点击此处上传</em>
-            </div>
-        </ElUpload>
-    </ElCard>
-</template>
 
-<script></script>
-<style></style>
+import UpFiles from '../../components/UpFiles.vue';
+<template>
+    <UpFiles></UpFiles>
+</template>
