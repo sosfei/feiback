adec2d488041dd6fb337dd3d8e398896eaf35043
sosfei <zingggum@outlook.com>
2023-12-17 23:02:49 +0800

    fix bug

--- src/main.js
+++ src/main.js
@@ -3,10 +3,10 @@ import { createApp } from 'vue'
 import Axios from 'axios'
 import App from '@/App.vue'
 import Router from '@/router'
-import UI from '@/library/index'
+import UIME from '@/library/index.js'
 
 Axios.defaults.baseURL = 'http://localhost:8881'
 const app = createApp(App)
+app.use(UIME)
 app.use(Router)
-app.use(UI)
 app.mount('#app')
